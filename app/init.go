7bc7599c2783d251b135bbe2cabf153c114778a0
Jeevanandam M <jeeva@myjeeva.com>
2018-11-18 03:15:01 -0800

    views and init file update

--- app/init.go
+++ app/init.go
@@ -19,10 +19,11 @@ package main
 
 import (
 	"aahframe.work"
-	// Registering HTML minifier
-	_ "aahframe.work/minify/html"
 
 	"thumbai.app/website/app/article"
+
+	// Registering HTML minifier
+	_ "aahframe.work/minify/html"
 )
 
 func init() {
@@ -31,40 +32,8 @@ func init() {
 	//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 	// Server Extensions
 	// Doc: https://docs.aahframework.org/server-extension.html
-	//
-	// Best Practice: Define a function with meaningful name in a package and
-	// register it here. Extensions function name gets logged in the log,
-	// its very helpful to have meaningful log information.
-	//
-	// Such as:
-	//    - Dedicated package for config loading
-	//    - Dedicated package for datasource connections
-	//    - etc
 	//__________________________________________________________________________
-
-	// Event: OnInit
-	// Doc: https://docs.aahframework.org/server-extension.html#event-oninit
-	//
-	// aah.OnInit(config.LoadRemote)
-
-	// Event: OnStart
-	// Doc: https://docs.aahframework.org/server-extension.html#event-onstart
-	//
-	// aah.OnStart(db.Connect)
-	// aah.OnStart(cache.Load)
-	app.OnStart(SubscribeHTTPEvents)
 	app.OnStart(article.LoadConfig)
-
-	// Event: OnPreShutdown
-	// Doc: https://docs.aahframework.org/server-extension.html#event-onpreshutdown
-	//
-	// aah.OnPreShutdown(notify.AnnounceImGonnaShutdown)
-
-	// Event: OnPostShutdown
-	// Doc: https://docs.aahframework.org/server-extension.html#event-onpostshutdown
-	//
-	// aah.OnPostShutdown(cache.Flush)
-	// aah.OnPostShutdown(db.Disconnect)
 
 	//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 	// Middleware's
@@ -79,88 +48,6 @@ func init() {
 		aah.BindMiddleware,
 		aah.AntiCSRFMiddleware,
 		aah.AuthcAuthzMiddleware,
-
-		//
-		// NOTE: Register your Custom middleware's right here
-		//
-
 		aah.ActionMiddleware,
 	)
-
-	//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-	// Add Application Error Handler
-	// Doc: https://docs.aahframework.org/error-handling.html
-	//__________________________________________________________________________
-	// aah.SetErrorHandler(AppErrorHandler)
-
-	//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-	// Add Custom Template Functions
-	// Doc: https://docs.aahframework.org/template-funcs.html
-	//__________________________________________________________________________
-	// aah.AddTemplateFunc(template.FuncMap{
-	// // ...
-	// })
-
-	//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-	// Add Custom Session Store
-	// Doc: https://docs.aahframework.org/session.html
-	//__________________________________________________________________________
-	// aah.AddSessionStore("redis", &RedisSessionStore{})
-
-	//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-	// Add Custom Value Parser
-	// Doc: https://docs.aahframework.org/request-parameters-auto-bind.html
-	//__________________________________________________________________________
-	// if err := aah.AddValueParser(reflect.TypeOf(CustomType{}), customParser); err != nil {
-	//   log.Error(err)
-	// }
-
-	//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-	// Add Custom Validation Functions
-	// Doc: https://godoc.org/gopkg.in/go-playground/validator.v9
-	//__________________________________________________________________________
-	// Obtain aah validator instance, then add yours
-	// validator := aah.Validator()
-	//
-	// // Add your validation funcs
-}
-
-//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-// HTTP Events
-//
-// Subscribing HTTP events on app start.
-//__________________________________________________________________________
-
-func SubscribeHTTPEvents(_ *aah.Event) {
-	// he := aah.AppHTTPEngine()
-
-	// Event: OnRequest
-	// Doc: https://docs.aahframework.org/server-extension.html#event-onrequest
-	//
-	// he.OnRequest(myserverext.OnRequest)
-
-	// Event: OnPreReply
-	// Doc: https://docs.aahframework.org/server-extension.html#event-onprereply
-	//
-	// he.OnPreReply(myserverext.OnPreReply)
-
-	// Event: OnHeaderReply
-	// Doc: https://docs.aahframework.org/server-extension.html#event-onheaderreply
-	//
-	// he.OnHeaderReply(myserverext.OnHeaderReply)
-
-	// Event: OnPostReply
-	// Doc: https://docs.aahframework.org/server-extension.html#event-onpostreply
-	//
-	// he.OnPostReply(myserverext.OnPostReply)
-
-	// Event: OnPreAuth
-	// Doc: https://docs.aahframework.org/server-extension.html#event-onpreauth
-	//
-	// he.OnPreAuth(myserverext.OnPreAuth)
-
-	// Event: OnPostAuth
-	// Doc: https://docs.aahframework.org/server-extension.html#event-onpostauth
-	//
-	// he.OnPostAuth(myserverext.PostAuthEvent)
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module thumbai.app/website
 
 require (
-	aahframe.work v0.0.0-20181103212526-cd7b92071338
+	aahframe.work v0.0.0-20181116070044-7e8c1f894ed3
 	aahframe.work/minify/html v0.0.0-20181103214207-e165c0fdbcbf
 	github.com/russross/blackfriday v1.5.2
 	github.com/shurcooL/sanitized_anchor_name v0.0.0-20170918181015-86672fcb3f95 // indirect
